c71b7261db719b167b4d89ce884fa3e7be010503
whitejeric <whitejeric@gmail.com>
2016-07-05 10:05:50 -0700

    Update five-day-weather-api.js

--- util/five-day-weather-api.js
+++ util/five-day-weather-api.js
@@ -1,6 +1,6 @@
 var Fetch = require('whatwg-fetch');
 var rootUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-var apiUrl = '&appid=bfb3f70a8cf06a906ea097f79fba35e3';
+var apiUrl = '&appid=OPEN_WEATHER_API_KEY';
 
 module.exports = {
   get: function(place){
